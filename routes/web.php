60fb09e3295ccd87ede5a4cac699f73dbe8318cd
Viniilinoo <viniciuslino2105@gmail.com>
2024-11-26 18:06:07 -0300

    first commit

---
+++ routes/web.php
@@ -0,0 +1,14 @@
+<?php
+
+use App\Http\Controllers\UserController;
+use Illuminate\Support\Facades\Route;
+
+Route::get('/cadastro_usuario', [UserController::class, 'formCriarUsuario']);
+
+Route::get('/listar_usuarios', [UserController::class, 'listar']);
+
+Route::post('/criar_usuario', [UserController::class, 'criar']);
+
+Route::post('/criar_produto', [ProdutoController::class, 'criar']);
+
+Route::get('/listar_produto', [ProdutoController::class, 'listar']);
